7fde015a542cb4ef44bdd8876d9dfa9f51af792e
José Luis A. Higuera <joeyhiguera91@gmail.com>
2021-08-18 05:28:46 -0500

    feature: get a flat array of children or parent nodes using QueryBuilder

--- app/Models/nodes.php
+++ app/Models/nodes.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\DB;
+
 class nodes extends Model
 {
     use HasFactory;
@@ -24,6 +26,41 @@ class nodes extends Model
 
     public function allParentsNodes() {
         return $this->parentsNodes()->with('allParentsNodes');
+    }
+
+
+    public static function allChildrenFluent($target_node_id)   {
+        return DB::select("WITH RECURSIVE node_tree (id, parent_id, 
+                    created_at, updated_At, deleted_at, name, path) AS ( 
+            SELECT id, parent_id, created_At, 
+                    updated_at, deleted_At, name, CAST(id as CHAR(200))
+            from nodes
+            WHERE id = :target_node
+            UNION ALL
+            SELECT node.id, node.parent_id,  
+                    node.created_At, node.updated_at, 
+                    node.deleted_At, node.name, 
+                    CONCAT(node_tree.path, ',', node.id)
+            FROM node_tree as node_tree JOIN nodes AS node on node_tree.id = node.parent_id
+        )
+        SELECT id, name, created_at, updated_at, deleted_at FROM node_tree;", [$target_node_id]);
+    }
+
+    public static function allParentsFluent($target_node_id)    {
+        return DB::select("WITH RECURSIVE node_tree (id, parent_id, 
+                    created_at, updated_At, deleted_at, name, path) AS ( 
+            SELECT id, parent_id, created_At, 
+                    updated_at, deleted_At, name, CAST(id as CHAR(200))
+            from nodes
+            WHERE id = :target_node
+            UNION ALL
+            SELECT node.id, node.parent_id,  
+                    node.created_At, node.updated_at, 
+                    node.deleted_At, node.name, 
+                    CONCAT(node_tree.path, ',', node.id)
+            FROM node_tree as node_tree JOIN nodes AS node on node_tree.parent_id = node.id
+        )
+        SELECT id, name, created_at, updated_at, deleted_at FROM node_tree;", [$target_node_id]);
     }
 
 }
