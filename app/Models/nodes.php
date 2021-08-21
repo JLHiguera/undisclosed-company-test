63eda5d991c7aa395e93fe9c26d21b112a2c60bb
José Luis A. Higuera <joeyhiguera91@gmail.com>
2021-08-21 04:36:47 -0500

    Refactor: Clean unneeded import in nodes model

--- app/Models/nodes.php
+++ app/Models/nodes.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-use App\Http\Resources\NodeResource;
-
 class nodes extends Model
 {
     use HasFactory;
