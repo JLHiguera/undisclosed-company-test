f8596f9ba868c40c2c9e9549f06b9b274f8bc799
José Luis A. Higuera <joeyhiguera91@gmail.com>
2021-08-21 04:22:51 -0500

    Fix: removes unneded code, adds paginated, flat node tree, queries

--- app/Models/nodes.php
+++ app/Models/nodes.php
@@ -5,26 +5,64 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+use App\Http\Resources\NodeResource;
+
 class nodes extends Model
 {
     use HasFactory;
-    public function childrenNodes()
-    {
-        return $this->hasMany('App\models\nodes', 'parent_id', 'id');
+
+    public static function parent_nodes($target_node_id) {
+        $recursive_query = self::target_node_query($target_node_id)
+                          ->unionAll(
+                              self::parents_recursive_query()
+                          );
+
+        return self::recursive_tree_query($recursive_query);                      
     }
 
-    public function allChildrenNodes()
-    {
-        return $this->childrenNodes()->with('allChildrenNodes');
+    public static function child_nodes($target_node_id) {
+        $recursive_query = self::target_node_query($target_node_id)
+                          ->unionAll(
+                              self::children_recursive_query()
+                          );
+
+        return self::recursive_tree_query($recursive_query);                      
     }
 
-    public function parentsNodes() {
-        return $this->belongsTo('App\models\nodes', 'parent_id', 'id');
+    private static function target_node_query($target_node_id) {
+        return DB::table("nodes")
+            ->where("id", $target_node_id)
+            ->selectRaw("id, parent_id, created_at, 
+            updated_at, deleted_At, name, CAST(id as CHAR(200))");
+                
     }
 
-    public function allParentsNodes() {
-        return $this->parentsNodes()->with('allParentsNodes');
+    private static function recursive_tree_query($recursive_query) {
+        return DB::table("node_tree")
+        ->select("id", "name", "created_at", "updated_at", "deleted_at" )
+        ->withRecursiveExpression("node_tree", $recursive_query, ["id", "parent_id", 
+        "created_at", "updated_at", "deleted_at", "name", "path"]);
     }
+
+    private static function parents_recursive_query() {
+        return  DB::table("nodes")
+        ->selectRaw("nodes.id, nodes.parent_id, nodes.created_At, 
+        nodes.updated_at, nodes.deleted_At, nodes.name, CONCAT(node_tree.path, ',', nodes.id)")
+        ->join("node_tree", "node_tree.parent_id", "=", "nodes.id");
+    }
+
+    private static function children_recursive_query() {
+        return  DB::table("nodes")
+        ->selectRaw("nodes.id, nodes.parent_id, nodes.created_At, 
+        nodes.updated_at, nodes.deleted_At, nodes.name, CONCAT(node_tree.path, ',', nodes.id)")
+        ->join("node_tree", "node_tree.id", "=", "nodes.parent_id");
+    }
+
+
+
 
 }
 
