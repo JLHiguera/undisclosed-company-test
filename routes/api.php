443a5021784a539c5b7b5e23dbf56593e60bda60
José Luis A. Higuera <joeyhiguera91@gmail.com>
2021-08-21 04:26:07 -0500

    New: adds children and parents routes for nodes under api/v1

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\NodeController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+//TODO add namespace and move version 1 controller to an appropiate folder under v1
+
+
+Route::prefix("/v1/")->group(function() {
+    Route::prefix("/node/{node_id}")->group(function() {
+        Route::get("/children", [NodeController::class, "children"]);    
+        Route::get("/parents", [NodeController::class, "parents"]);
+    });
 });
+
